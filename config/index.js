d0a511447c90688ab4139c52fc84767cc3bef6e5
myhlcb <1029785965@qq.com>
2019-09-29 18:03:25 +0800

    feat():update .gitignore

--- config/index.js
+++
@@ -1,4 +0,0 @@
-const _ = require ('lodash');
-const env = process.env.NODE_ENV || 'dev';
-console.log (Object.assign ({}, require (`./${env}`)), 1111);
-module.exports = Object.assign ({}, require (`./${env}`));
